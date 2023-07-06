42bfb2dcace24242351c5757bfb78a6a101f4e04
karlmark <karlmark@karl.com>
2023-07-06 23:26:45 +0800

    new

---
+++ src/main/java/com/cry/Test.java
@@ -0,0 +1,89 @@
+package com.cry;
+import java.lang.reflect.Field;
+interface I1 {
+}
+interface I2 {
+}
+class Cell{
+    public int mCellPublic;
+}
+class Animal extends  Cell{
+    private int mAnimalPrivate;
+    protected int mAnimalProtected;
+    int mAnimalDefault;
+    public int mAnimalPublic;
+    private static int sAnimalPrivate;
+    protected static int sAnimalProtected;
+    static int sAnimalDefault;
+    public static int sAnimalPublic;
+}
+class Dog extends Animal implements I1, I2 {
+    private int mDogPrivate;
+    public int mDogPublic;
+    protected int mDogProtected;
+    private int mDogDefault;
+    private static int sDogPrivate;
+    protected static int sDogProtected;
+    static int sDogDefault;
+    public static int sDogPublic;
+}
+public class Test {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+//        Class<Dog> dog = Dog.class;
+        Class dog = Dog.class;
+        //类名打印
+        System.out.println(dog.getName()); //com.cry.Dog
+        System.out.println(dog.getSimpleName()); //Dog
+        System.out.println(dog.getCanonicalName());//com.cry.Dog
+
+        // forName
+
+        System.out.println(Class.forName("com.cry.I1")); // interface com.cry.I1
+        System.out.println(Class.forName("com.cry.Dog")); // class com.cry.Dog
+        System.out.println(Class.forName("com.cry.Animal")); // class com.cry.Animal
+        System.out.println(Class.forName("com.cry.Test")); // class com.cry.Test
+
+        //接口
+        System.out.println(dog.isInterface()); //false
+        for (Class iI : dog.getInterfaces()) {
+            System.out.println(iI);
+        }
+         /*
+          interface com.cry.I1
+          interface com.cry.I2
+         */
+
+        //父类
+        System.out.println(dog.getSuperclass());//class com.cry.Animal
+        //创建对象
+        Dog d = (Dog) dog.newInstance();
+        d.mAnimalDefault = 1;
+
+        System.out.println(d.mAnimalDefault);
+        //字段
+        for (Field f : dog.getFields()) {
+            System.out.println(f.getName());
+        }
+        /*
+            mDogPublic
+            sDogPublic
+            mAnimalPublic
+            sAnimalPublic
+            mCellPublic  //父类的父类的公共字段也打印出来了
+         */
+        System.out.println("---------");
+        for (Field f : dog.getDeclaredFields()) {
+            System.out.println(f.getName());
+        }
+        /** 只有自己类声明的字段
+         mDogPrivate
+         mDogPublic
+         mDogProtected
+         mDogDefault
+         sDogPrivate
+         sDogProtected
+         sDogDefault
+         sDogPublic
+         */
+    }
+}
---
+++ src/main/java/input_word.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class input_word {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int i = sc.nextInt();
+
+        System.out.println(i);
+
+        main2();
+    }
+
+    public static void main2(){
+        System.out.println(123);
+    }
+}
